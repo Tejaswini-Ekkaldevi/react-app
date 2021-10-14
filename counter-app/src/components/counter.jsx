6491d00597f2d436cc0ae50c1ccfec426aab8320
Tejaswini Ekkaldevi <tejaswini.ekkaldevi@indexnine.com>
2021-10-14 19:06:56 +0530

    Counter

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+    state = {
+        count: this.props.counter.value,
+        // tags: ['tag1', 'tag2', 'tag3']
+    };
+   // constructor() {
+     //   super();
+       // this.handleIncrement = this.handleIncrement.bind(this);
+   // }
+   /* handleIncrement = product => {
+        console.log(product)
+       // console.log('Increment Clicked', this);
+        this.setState({count: this.state.count + 1});
+    }*/
+
+//   renderTags(){
+  //      if(this.state.tags === 0) return <p>There is no tags.</p>
+   //     return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    //};
+    render() { 
+        console.log(this.props);
+        return (
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                  onClick={()=>this.props.onIncrement(this.props.counter)}
+                 className="btn btn-secondary btn-sm"> Increment</button>
+                 <button onClick={() => this.props.onDelete(this.props.counter.id)}
+                 className="btn btn-danger btn-sm m-2">Delete</button>
+            </div>
+        );
+    };
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.props.counter === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount() {
+        const { count } =this.props.counter;
+        return count ===0 ? 'Zero' : count;
+    }
+}
+export default Counter;
+/*
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount() {
+        const { count } =this.state;
+        return count ===0 ? 'Zero' : count;
+    }
+*/
